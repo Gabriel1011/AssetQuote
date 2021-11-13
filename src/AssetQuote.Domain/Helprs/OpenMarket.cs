b5ead090637cf3de78780d977bce847896af91db
Gabriel Rodrigues <38117500+Gabriel1011@users.noreply.github.com>
2021-11-13 16:45:51 -0300

    Regra pra verificar se o mercado está aberto.

---
+++ src/AssetQuote.Domain/Helprs/OpenMarket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AssetQuote.Domain.Helprs
+{
+    public static class OpenMarket
+    {
+        public static bool CheckOpenMarket() 
+        {
+            var today = DateTime.Now;
+
+            return (today.Hour > 9 && today.Hour < 18) && !(today.DayOfWeek == DayOfWeek.Sunday || today.DayOfWeek == DayOfWeek.Saturday);
+        } 
+    }
+}
