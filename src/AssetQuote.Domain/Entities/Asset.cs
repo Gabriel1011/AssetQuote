3cd371fe3f33ca154884edd199858e96d1807eed
Gabriel Rodrigues <38117500+Gabriel1011@users.noreply.github.com>
2021-12-05 14:38:47 -0300

    Ajustando nome das propriedades da entidade "Asset"

--- src/AssetQuote.Domain/Entities/Asset.cs
+++ src/AssetQuote.Domain/Entities/Asset.cs
@@ -4,8 +4,8 @@ public class Asset : BaseEntity
 {
     public string Name { get; set; }
     public string Code { get; set; }
-    public double Valor { get; set; }
-    public double Porcentagem { get; set; }
-    public double ValorOcilacao { get; set; }
+    public double Value { get; set; }
+    public double Percentage { get; set; }
+    public double OcillationValue { get; set; }
     public List<BotThread> BotThreads { get; set; }
 }
